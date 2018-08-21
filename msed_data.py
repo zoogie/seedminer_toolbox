7e70080d0f053fb201bcec98368cca41a912c706
zoogie <secretAgentCokey@gmail.com>
2018-08-21 07:14:07 -0500

    old naming derps

--- msed_data.py
+++ msed_data.py
@@ -5,13 +5,13 @@ buf=f.read()
 f.close()
 o=0x110
 lfcs=struct.unpack("<I",buf[o+0:o+4])[0]
-msed2=struct.unpack("<I",buf[o+4:o+8])[0]
+msed1=struct.unpack("<I",buf[o+4:o+8])[0]
 msed3=struct.unpack("<I",buf[o+12:o+16])[0]&0x7FFFFFFF
-print(hex(lfcs),hex(msed2),hex(msed3))
+print(hex(lfcs),hex(msed1),hex(msed3))
 
 est=lfcs//5-msed3
 isnew=0
-if(msed2):
+if(msed1):
 	isnew=1
 print(hex(est))
 print(est)
